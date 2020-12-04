2ef0edd62bd749785d2fda8cfbad22dc91e48722
bill080307 <bill080307@live.cn>
2020-12-04 22:33:30 +0800

    init webui

---
+++ webui/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="home">
+    <b-container>
+
+    </b-container>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'home',
+    data(){
+      return {
+      }
+    },
+    methods:{
+      async init(){
+      },
+    },
+    created() {
+      this.init()
+    },
+  }
+</script>
+<style>
+
+</style>
