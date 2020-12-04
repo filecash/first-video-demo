60d36ea2838e790d98913155265bda4fd0a980ec
bill080307 <bill080307@live.cn>
2020-12-04 22:10:55 +0800

    install nginx

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+#install nginx
+wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
+apt -y install software-properties-common
+add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
+apt update
+apt install openresty
