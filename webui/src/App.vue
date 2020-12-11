5df900998a7a8c34223a54d64813c8abc934f199
bill080307 <bill080307@live.cn>
2020-12-11 22:06:23 +0800

    update Ui

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div>
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-brand to="/">FileCash Video Demo</b-navbar-brand>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <b-container>
+      <router-view/>
+    </b-container>
   </div>
 </template>
 
--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -1,8 +1,30 @@
 <template>
   <div id="home">
-    <b-container>
-
-    </b-container>
+    <b-row>
+      <b-col>
+        <b-form-group label-cols="3" label-cols-xl="1" label-cols-lg="1" label-cols-md="2" label-size="lg" label="网络:" label-for="select-network">
+          <b-form-select id="select-network" v-model="select_network" :options="networks" disabled></b-form-select>
+        </b-form-group>
+        <hr>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col xl="3" lg="4" md="12">
+        <b-form-group label-cols="3" label-cols-xl="3" label-cols-lg="3" label-cols-md="2"  label-size="lg" label="类别:" label-for="select-genre">
+          <b-form-select id="select-genre" v-model="select_genre" :options="genres"></b-form-select>
+        </b-form-group>
+      </b-col>
+      <b-col xl="6" lg="8" md="12">
+        <b-form-group label-cols="3" label-cols-xl="2" label-cols-lg="2"  label-cols-md="2" label-size="lg" label="文件:" label-for="select-file">
+          <b-form-select id="select-file" v-model="select_file" :options="files"></b-form-select>
+        </b-form-group>
+      </b-col>
+      <b-col xl="3" lg="12" md="12">
+        <b-form-group label-cols="3" label-cols-xl="3" label-cols-lg="1" label-cols-md="2" label-size="lg" label="通道:" label-for="select-vod">
+          <b-form-select id="select-vod" v-model="select_vod" :options="vods"></b-form-select>
+        </b-form-group>
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
@@ -10,10 +32,32 @@
     name: 'home',
     data(){
       return {
+        networks:[
+          {
+            "text":"免费网络 - IPFS",
+            "value":"ipfs"
+          },
+          {
+            "text":"支付网络 - FileCache",
+            "value":"filecash"
+          },
+          {
+            "text":"支付网络 - FileCoin",
+            "value":"filecoin"
+          },
+        ],
+        select_network: null,
+        genres:[],
+        select_genre: null,
+        files:[],
+        select_file: null,
+        vods:[],
+        select_vod: null,
       }
     },
     methods:{
       async init(){
+        this.select_network = this.networks[0].value;
       },
     },
     created() {
@@ -22,5 +66,7 @@
   }
 </script>
 <style>
-
+#home{
+  padding-top: 15px;
+}
 </style>
