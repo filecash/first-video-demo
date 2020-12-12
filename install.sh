ce09ae07b1d5aca24234d77f3164a712bdbbec5d
bill080307 <bill080307@live.cn>
2020-12-12 08:51:49 +0800

    add deploy

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+#build web ui
+cd webui
+npm install
+npm run build
+
+#publish web ui
+cd dist/
+tar -zcvf ../webui.tar.gz ./*
+cd ..
+
+#install web ui
+tar -zxvf webui.tar.gz -C /var/www/html/
