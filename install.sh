28edc0e6a701e33bd624e31ba8bc41e410cce2da
bill080307 <bill080307@live.cn>
2020-12-28 19:28:18 +0800

    update package

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
 apt -y install software-properties-common
 add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
 apt update
-apt install openresty
+apt install -y openresty redis-server
